6f38a99d778302eda4bf7047a2dd1d9ff2947aa8
Mohanatheesan Theiventhiram <48876593+Mohanatheesan26@users.noreply.github.com>
2020-09-12 12:48:53 +0530

    Update FileRead.java
    FileRead Class Updated

--- Log Analyser/src/main/java/com/assignment/fileOperations/FileRead.java
+++ Log Analyser/src/main/java/com/assignment/fileOperations/FileRead.java
@@ -1,33 +1,16 @@
 package com.assignment.fileOperations;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class FileRead {
 
-    public String readFile(String path) {
+    public Scanner readFile(String path) {
         try
         {
-            File file=new File(path);
-            FileReader fr=new FileReader(file);
-            BufferedReader br=new BufferedReader(fr);
-            StringBuffer sb=new StringBuffer();
-            String line;
-            while((line=br.readLine())!=null)
-            {
-                sb.append(line);
-                boolean isERROR = line.contains("ERROR");
-                if(isERROR){
-                    System.out.println("Error occured send mail");
-                    break;
-                }
-                sb.append("\n");
-            }
-            fr.close();
-            System.out.println(sb.toString());
-            return sb.toString();
+            FileInputStream file = new FileInputStream(path);
+            Scanner scanner = new Scanner(file);
+            return scanner;
         }
 
         catch(IOException e)
