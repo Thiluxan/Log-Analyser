19a75066e000fc386a7bb311fef44b65a34d61c5
Thiluxan Subramaniam <thiluxan@hotmail.co.uk>
2020-09-13 19:25:34 +0530

    Main class updated

--- Log Analyser/src/main/java/com/assignment/Main.java
+++ Log Analyser/src/main/java/com/assignment/Main.java
@@ -1,15 +1,33 @@
 package com.assignment;
 
+import com.assignment.database.DatabaseConnection;
+import com.assignment.database.EmailRepository;
 import com.assignment.email.MailConfiguration;
-import com.assignment.operation.LogOperation;
+import com.assignment.email.MailSending;
+import com.assignment.fileOperations.FileExist;
+import com.assignment.fileOperations.FileRead;
+import com.assignment.fileOperations.FileWrite;
+import com.assignment.input.CommandLineInput;
+import com.assignment.input.Input;
+import com.assignment.operations.LogOperation;
+import com.assignment.operations.TimeStamp;
 
 public class Main {
     public static void main(String args[]) throws Exception{
-        LogOperation log = new LogOperation();
-        log.performOperation();
-
+        LogOperation logOperation = new LogOperation();
+        FileExist fileExist = new FileExist();
+        FileRead fileRead  = new FileRead();
+        FileWrite fileWrite = new FileWrite();
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        EmailRepository emailRepository = new EmailRepository();
         MailConfiguration mailConfiguration = new MailConfiguration();
-        mailConfiguration.configureMail("luxanmani@gmail.com");
+        MailSending mailSending = new MailSending();
+        Input input = new CommandLineInput();
+        TimeStamp timeStamp = new TimeStamp();
+
+        LogAnalyzerApp logAnalyzerApp = new LogAnalyzerApp(fileRead,fileWrite,fileExist,databaseConnection,
+                emailRepository,mailConfiguration,input,logOperation,timeStamp, mailSending);
+        logAnalyzerApp.execute();
 
     }
 }
