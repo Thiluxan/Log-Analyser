55f0f413f7f1642ec8c934d39b1fe73dcca7b62d
Thiluxan Subramaniam <thiluxan@hotmail.co.uk>
2020-09-15 13:31:40 +0530

    Operations package updated

--- Log Analyser/src/main/java/com/assignment/operations/FileExistOperation.java
+++ Log Analyser/src/main/java/com/assignment/operations/FileExistOperation.java
@@ -5,35 +5,44 @@ import com.assignment.email.MailSending;
 import com.assignment.fileOperations.FileWrite;
 import com.assignment.timeStamp.SavedTimeStamp;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
-public class FileExistOperation implements Operation{
+public class FileExistOperation implements Operation {
     @Override
     public void performOperation(FileWrite fileWrite, Scanner scanner, SavedTimeStamp savedTimeStamp,
                                  MailConfiguration mailConfiguration, MailSending mailSending, List<String> emailList,
-                                 List<String> timeStampList) {
+                                 List<String> timeStampList) throws IOException {
+
         String logLine;
+        int flag = 0;
         int index;
         String outputPath = "E:\\Java\\Log Analyser\\src\\main\\resources\\output.txt";
+
         while (scanner.hasNextLine()){
             logLine = scanner.nextLine();
-            if(logLine.contains("ERROR")){
-                index = timeStampList.indexOf(logLine.split(" ")[0]);
-                for(int i=index+1; i<timeStampList.size(); i++){
-                    if(timeStampList.get(i).equals(timeStampList.get(index))){
-                        continue;
+            if(flag==1){
+                if(logLine.contains("ERROR")){
+                    index = timeStampList.indexOf(logLine.split(" ")[0]);
+                    for(int i=index+1; i<timeStampList.size(); i++){
+                        if(timeStampList.get(i).equals(timeStampList.get(index))){
+                            continue;
+                        }
+                        else{
+                            index = i;
+                            break;
+                        }
                     }
-                    else{
-                        index = i;
-                        break;
-                    }
+                    fileWrite.writeFile(timeStampList.get(index),outputPath);
+                    //mailSending.sendMail(mailConfiguration,emailList);
+                    break;
                 }
-                fileWrite.writeFile(timeStampList.get(index),outputPath);
-                //mailSending.sendMail(mailConfiguration,emailList);
-                break;
+            }
+
+            if(logLine.split(" ")[0].equals(savedTimeStamp.getTimeStamp())){
+                flag = 1;
             }
         }
-
     }
 }
--- Log Analyser/src/main/java/com/assignment/operations/FileNotExistOperation.java
+++ Log Analyser/src/main/java/com/assignment/operations/FileNotExistOperation.java
@@ -1,48 +1,40 @@
 package com.assignment.operations;
 
-import com.assignment.email.MailConfiguration;
-import com.assignment.email.MailSending;
-import com.assignment.fileOperations.FileWrite;
-import com.assignment.timeStamp.SavedTimeStamp;
+        import com.assignment.email.MailConfiguration;
+        import com.assignment.email.MailSending;
+        import com.assignment.fileOperations.FileWrite;
+        import com.assignment.timeStamp.SavedTimeStamp;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Scanner;
+        import java.util.List;
+        import java.util.Scanner;
 
-public class FileNotExistOperation implements Operation {
+public class FileNotExistOperation implements Operation{
     @Override
     public void performOperation(FileWrite fileWrite, Scanner scanner, SavedTimeStamp savedTimeStamp,
                                  MailConfiguration mailConfiguration, MailSending mailSending, List<String> emailList,
-                                 List<String> timeStampList) throws IOException {
-
+                                 List<String> timeStampList) {
         String logLine;
-        int flag = 0;
         int index;
         String outputPath = "E:\\Java\\Log Analyser\\src\\main\\resources\\output.txt";
-
         while (scanner.hasNextLine()){
             logLine = scanner.nextLine();
-            if(flag==1){
-                if(logLine.contains("ERROR")){
-                    index = timeStampList.indexOf(logLine.split(" ")[0]);
-                    for(int i=index+1; i<timeStampList.size(); i++){
-                        if(timeStampList.get(i).equals(timeStampList.get(index))){
-                            continue;
-                        }
-                        else{
-                            index = i;
-                            break;
-                        }
+            if(logLine.contains("ERROR")){
+                index = timeStampList.indexOf(logLine.split(" ")[0]);
+                for(int i=index+1; i<timeStampList.size(); i++){
+                    if(timeStampList.get(i).equals(timeStampList.get(index))){
+                        continue;
                     }
-                    fileWrite.writeFile(timeStampList.get(index),outputPath);
-                    //mailSending.sendMail(mailConfiguration,emailList);
-                    break;
+                    else{
+                        index = i;
+                        break;
+                    }
                 }
-            }
-
-            if(logLine.split(" ")[0].equals(savedTimeStamp.getTimeStamp())){
-                flag = 1;
+                fileWrite.writeFile(timeStampList.get(index),outputPath);
+                //mailSending.sendMail(mailConfiguration,emailList);
+                break;
             }
         }
+
     }
 }
+
