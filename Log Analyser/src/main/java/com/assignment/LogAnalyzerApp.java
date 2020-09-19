60f6b93571eec2b351dfbd1644e96d44d525fac2
Thiluxan Subramaniam <thiluxan@hotmail.co.uk>
2020-09-19 18:00:10 +0530

    Update LogAnalyzerApp.java

--- Log Analyser/src/main/java/com/assignment/LogAnalyzerApp.java
+++ Log Analyser/src/main/java/com/assignment/LogAnalyzerApp.java
@@ -1,7 +1,7 @@
 package com.assignment;
 
+import com.assignment.database.DataRepository;
 import com.assignment.database.DatabaseConnection;
-import com.assignment.database.EmailRepository;
 import com.assignment.email.MailConfiguration;
 import com.assignment.email.MailSending;
 import com.assignment.fileOperations.FileExist;
@@ -10,8 +10,8 @@ import com.assignment.fileOperations.FileWrite;
 import com.assignment.input.Input;
 import com.assignment.operations.Operation;
 import com.assignment.operations.OperationFactory;
+import com.assignment.timeStamp.LogRepository;
 import com.assignment.timeStamp.SavedTimeStamp;
-import com.assignment.timeStamp.TimeStampRepository;
 
 import java.io.IOException;
 import java.util.List;
@@ -23,18 +23,18 @@ public class LogAnalyzerApp {
     private FileWrite fileWrite;
     private FileExist fileExist;
     private DatabaseConnection databaseConnection;
-    private EmailRepository emailRepository;
+    private DataRepository emailRepository;
     private MailConfiguration mailConfiguration;
     private Input input;
     private SavedTimeStamp savedTimeStamp;
     private MailSending mailSending;
-    private TimeStampRepository timeStampRepository;
+    private LogRepository timeStampRepository;
     private OperationFactory operationFactory;
 
     public LogAnalyzerApp(FileRead fileRead, FileWrite fileWrite, FileExist fileExist, DatabaseConnection databaseConnection,
-                          EmailRepository emailRepository, MailConfiguration mailConfiguration, Input input,
+                          DataRepository emailRepository, MailConfiguration mailConfiguration, Input input,
                           OperationFactory operationFactory, SavedTimeStamp savedTimeStamp, MailSending mailSending,
-                          TimeStampRepository timeStampRepository) {
+                          LogRepository timeStampRepository) {
         this.fileRead = fileRead;
         this.fileWrite = fileWrite;
         this.fileExist = fileExist;
@@ -49,22 +49,38 @@ public class LogAnalyzerApp {
     }
 
     public void execute() throws IOException, NullPointerException {
+        //Getting list of emails
         databaseConnection.connectDatabase();
-        List<String> emailList = emailRepository.getEmails();
-
+        List<String> emailList = emailRepository.getData();
+        
+        String outputPath = "E:\\Java\\Log Analyser\\src\\main\\resources\\output.txt";
+        
+        // Getting the timeStamp already saved
+        boolean isOutputFileExist = fileExist.isFileExists(outputPath);
+        Scanner outputFileScanner = fileRead.readFile(outputPath);
+        String errorTimeStamp = savedTimeStamp.getTimeStamp(isOutputFileExist,outputFileScanner);
+        
+        //Getting user input
         String path = input.getInput();
 
         Scanner scanner = fileRead.readFile(path);
-
+        
+        //Getting List of timeStamps in the log file
         List<String> timeStampList = timeStampRepository.getTimeStamp(scanner);
 
         Scanner scanner1 = fileRead.readFile(path);
 
-        boolean isFileExists = fileExist.isFileExists("\"E:\\\\Java\\\\Log Analyser\\\\src\\\\main\\\\resources\\\\output.txt\"");
+        boolean isFileExists = fileExist.isFileExists(outputPath);
 
         Operation operation = operationFactory.getInstance(isFileExists);
 
-        operation.performOperation(fileWrite,scanner1,savedTimeStamp,mailConfiguration,mailSending,emailList,timeStampList);
+        //Finding the error in the log file
+        String output = operation.performOperation(scanner1,outputPath,timeStampList);
+
+        if(output.length() != 0){
+            fileWrite.writeFile(output,outputPath);
+            mailSending.sendMail(mailConfiguration,emailList);
+        }
 
     }
 }
